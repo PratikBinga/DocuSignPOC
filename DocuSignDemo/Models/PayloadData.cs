70cbf12498aef5cd59f1331c47f72789c3f31af7
PratikBinga <42069638+PratikBinga@users.noreply.github.com>
2018-12-26 02:52:31 +0530

    adding paylod class

---
+++ DocuSignDemo/Models/PayloadData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DocuSignDemo.Models
+{
+    public class PayloadData
+    {
+        public string DocumentBase64 { get; set; }
+
+        public string UserNameFile { get; set; }
+    }
+}
